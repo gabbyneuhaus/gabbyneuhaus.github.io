d1b99af7a75c7b5e07b2bfcb456dcd260e16aea0
gabbyneuhaus <gncreativesolutions@gmail.com>
2023-07-23 20:03:18 -0400

    contact page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ $(document).ready(function() {
 
     });
 });
+function validateForm(event) {
+    event.preventDefault(); // Prevent the form from being submitted by default.
+  
+    const emailInput = document.getElementById('email');
+    const messageInput = document.getElementById('message');
+    const errorMessages = document.getElementById('errorMessages');
+      // Clear any existing error messages.
+  errorMessages.textContent = '';
+
+  // Validate email
+  if (!isValidEmail(emailInput.value)) {
+    errorMessages.textContent += 'Please enter a valid email address.\n';
+    emailInput.focus();
+    return false;
+  }
+
+  // Validate message
+  if (!messageInput.value.trim()) {
+    errorMessages.textContent += 'Please enter a message.\n';
+    messageInput.focus();
+    return false;
+  }
+
+  // If both email and message are valid, the form can be submitted.
+  alert('Form submitted successfully!');
+}
+
+function isValidEmail(email) {
+  // A basic email validation using a regular expression.
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
